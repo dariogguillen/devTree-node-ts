3f9754c8d843602a6072e9cab76d522d16cd4050
Darío Guillén <guillendario@gmail.com>
2025-05-28 13:48:06 -0600

    feat: using colors for better logs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from "colors";
 import server from "./server";
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log("Server Working...");
+  console.log(colors.blue.italic(`Server working on port: ${PORT}`));
 });
