7fa7c9be4f6c79afacb3b2afa06932a786c78a8e
Darío Guillén <guillendario@gmail.com>
2025-05-28 21:11:35 -0600

    feat: validation in router added

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,25 +2,32 @@ import { Request, Response } from "express";
 import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
+import { validationResult } from "express-validator";
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email, username, password } = req.body;
-  const emailExist = await User.findOne({ email });
-  const userName = slug(username, "");
-  const usernameExist = await User.findOne({ username: userName });
-
-  if (emailExist) {
-    const error = new Error("Email already in used");
-    res.status(409).json({ error: error.message });
-  } else if (usernameExist) {
-    const error = new Error("Username already in used");
-    res.status(409).json({ error: error.message });
+  // handle errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
   } else {
-    const user = new User(req.body);
-    user.password = await hashPassword(password);
-    user.username = userName;
-    await user.save();
+    const { email, username, password } = req.body;
+    const emailExist = await User.findOne({ email });
+    const userName = slug(username, "");
+    const usernameExist = await User.findOne({ username: userName });
 
-    res.status(201).json({ response: "User created successfully" });
+    if (emailExist) {
+      const error = new Error("Email already in used");
+      res.status(409).json({ error: error.message });
+    } else if (usernameExist) {
+      const error = new Error("Username already in used");
+      res.status(409).json({ error: error.message });
+    } else {
+      const user = new User(req.body);
+      user.password = await hashPassword(password);
+      user.username = userName;
+      await user.save();
+
+      res.status(201).json({ response: "User created successfully" });
+    }
   }
 };
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 /** Authentication / Register */
 
-router.post("/auth/register", createAccount);
+router.post(
+  "/auth/register",
+  body("name").notEmpty().withMessage("Not empty name."),
+  body("lastName").notEmpty().withMessage("Not empty lastName."),
+  body("username").notEmpty().withMessage("Not empty username."),
+  body("email").isEmail().withMessage("Invalid email"),
+  body("password")
+    .isLength({ min: 12 })
+    .withMessage("Password length min 12 characters"),
+  createAccount,
+);
 
 export default router;
