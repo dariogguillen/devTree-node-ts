d86f1e3a85d55f436147db28475a87a17b79d15b
Darío Guillén <guillendario@gmail.com>
2025-05-28 20:50:38 -0600

    fix: update user and validate username

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from "express";
+import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
   const emailExist = await User.findOne({ email });
-  const usernameExist = await User.findOne({ username });
+  const userName = slug(username, "");
+  const usernameExist = await User.findOne({ username: userName });
 
-  if (emailExist || usernameExist) {
-    const error = new Error("Username or email already in used");
+  if (emailExist) {
+    const error = new Error("Email already in used");
+    res.status(409).json({ error: error.message });
+  } else if (usernameExist) {
+    const error = new Error("Username already in used");
     res.status(409).json({ error: error.message });
   } else {
     const user = new User(req.body);
     user.password = await hashPassword(password);
+    user.username = userName;
     await user.save();
+
     res.status(201).json({ response: "User created successfully" });
   }
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,23 +1,37 @@
 import mongoose, { Schema } from "mongoose";
 
 export interface IUser {
+  name: string;
+  lastName: string;
   username: string;
   email: string;
   password: string;
 }
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   username: {
     type: String,
     required: true,
     trim: true,
     unique: true,
+    lowercase: true,
   },
   email: {
     type: String,
     required: true,
     trim: true,
     unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
