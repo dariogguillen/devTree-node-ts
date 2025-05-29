51d6f1c72531c2c142e90d8f438725eeb64d7342
Darío Guillén <guillendario@gmail.com>
2025-05-28 19:57:46 -0600

    refactor: router handlers added

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,10 @@
 import { Router } from "express";
-import User from "./models/User";
+import { createAccount } from "./handlers";
 
 const router = Router();
 
 /** Authentication / Register */
 
-router.post("/auth/register", async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-
-  res.send("Registro creado correctamente");
-});
+router.post("/auth/register", createAccount);
 
 export default router;
