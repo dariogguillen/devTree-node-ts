2217de269b8c277d25b07fa6564ccb12e2c972c4
Darío Guillén <guillendario@gmail.com>
2025-05-30 17:54:28 -0600

    fix: update cors to allow request from applications

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,9 @@ import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    origin === process.env.FRONTEND_URL
+    const withList = [process.env.FRONTEND_URL];
+    if (process.argv[2] === "--api") withList.push(undefined);
+    withList.includes(origin)
       ? callback(null, true)
       : callback(new Error("Not allowed by CORS"));
   },
