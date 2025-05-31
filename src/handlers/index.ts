698155baa21591aa649830c09e1aa60fd426b897
Darío Guillén <guillendario@gmail.com>
2025-05-31 13:45:38 -0600

    feat: verify jwt token

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import slug from "slug";
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
-import { generateJWT } from "../utils/jwt";
+import { generateJWT, verifyJWT } from "../utils/jwt";
 
 export const createAccount = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
@@ -43,4 +43,30 @@ export const login = async (req: Request, res: Response) => {
   }
   const token = generateJWT({ id: user._id });
   res.status(200).send({ response: "User logged in successfully", token });
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  const bearer = req.headers.authorization;
+  const [, token] = bearer ? bearer.split(" ") : [];
+
+  if (!bearer || !token) {
+    const error = new Error("Unauthorized");
+    res.status(401).json({ error: error.message });
+    return;
+  }
+
+  try {
+    if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET not found");
+    const verify = verifyJWT(token);
+    if (typeof verify === "object" && verify.id) {
+      const user = await User.findById(verify.id).select("-password");
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+      res.status(200).json({ response: "User found", user });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Invalid token" });
+  }
 };
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -7,3 +7,8 @@ export const generateJWT = (payload: JwtPayload) => {
   });
   return token;
 };
+
+export const verifyJWT = (token: string) => {
+  if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET not found");
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
